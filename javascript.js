78d0bcdf3212d78c96738724c047024e884a6af1
PaulaMaged <128493887+PaulaMaged@users.noreply.github.com>
2023-08-29 23:00:25 +0300

    Added game, 5 rounds

--- javascript.js
+++ javascript.js
@@ -5,19 +5,45 @@ function randomComputerChoice() {
     else return "scissors";
 }
 
-let computerChoice = randomComputerChoice();
-let userChoice = prompt("Choose").toLowerCase();
-
 function playRound(playerSelection, computerSelection) {
-    let state = "Lose";
+    let state = null;
     switch(true) {
 
         case playerSelection == computerSelection: return "It's a tie";
         case playerSelection == "rock" && computerSelection == "scissors":
         case playerSelection == "scissors" && computerSelection == "paper":
-        case playerSelection == "paper" && computerSelection == "rock": state = "Win";
+        case playerSelection == "paper" && computerSelection == "rock": state = "Win"; break;
+        case computerSelection == "rock" && playerSelection == "scissors":
+        case computerSelection == "scissors" && playerSelection == "paper":
+        case computerSelection == "paper" && playerSelection == "rock": state = "Lose";
 
     }
-    if(state == "Lose") return `You ${state}! ${computerSelection} beats ${playerSelection}`;
+    if(!state) return null;
+    else if(state == "Lose") return `You ${state}! ${computerSelection} beats ${playerSelection}`;
     else return `You ${state}! ${playerSelection} beats ${computerSelection}`;
     }
+
+
+function game() {
+    let c = 0;
+    let u = 0;
+    while(c != 5 && u != 5){
+        let computerChoice = randomComputerChoice();
+        let userChoice = prompt("Choose").toLowerCase();
+        let msg = playRound(userChoice, computerChoice);
+        while(!msg) {
+            console.log("type choices correctly");
+            userChoice = prompt("Choose").toLowerCase();
+            msg = playRound(userChoice, computerChoice);
+        }
+        console.log(msg);
+        let state = msg.charAt(4); 
+        switch(state){
+            case "L": c++; break;
+            case "W": u++; break;
+        }
+        console.log(`score is user: ${u} vs computer: ${c}`)
+    }
+    if(c == 5) console.log("You Lost the Game");
+    else console.log("You Won the Game, Congratulations");
+}
